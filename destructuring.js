2018a631e574eb4e2b205683285eaa817c67927f
Nick <nickdekok.nl@gmail.com>
2023-04-25 13:13:28 +0200

    opdrachten gemaakt

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,11 @@ const product = {
   screenType: 'LED',
 }
 
+const {price, refreshRate} = product
+
+console.log(price)
+console.log(refreshRate)
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +22,11 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+let {type, brand} = productInformation.general
+
+console.log(type)
+console.log(brand)
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -38,15 +48,24 @@ const tvOptions = {
   },
 };
 
+const {wifi, bluetooth} = tvOptions.options.connectivity.popular
+console.log(wifi)
+console.log(bluetooth)
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
+const [first, second] = connectivity
+console.log(first)
+console.log(second)
 
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
-
+const [firstTV, , thirdTV] = tvSizes
+console.log(firstTV)
+console.log(thirdTV)
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
@@ -57,8 +76,18 @@ function getCompanyDetails() {
   }
 }
 
+const details = getCompanyDetails()
+const {name, adress} = details
+console.log(name)
+console.log(adress)
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const sentences = getDetails('Nick', 32)
+const [sentence1, sentence2] = sentences
+console.log(sentence1)
+console.log(sentence2)
